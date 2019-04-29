6ea093f87904a54419ee51ac3bf3a8785974d72c
Gian Libunao <gianlibunao@gmail.com>
2019-04-29 02:05:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  newArray = []
+  counter = 0
+  while counter<array.length
+  if array[counter].split(" ").length>1
+    newArray << array[counter].split(" ").first
+  else
+    newArray << array[counter].upcase
+  end
+  counter+=1
+  end
+newArray
+end
 
