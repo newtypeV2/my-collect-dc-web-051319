0a522cc9fb875adc1429b0dc816c24928a4c5c5f
Gian Libunao <gianlibunao@gmail.com>
2019-04-29 02:07:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,12 @@ def my_collect(array)
   newArray = []
   counter = 0
   while counter<array.length
-  if array[counter].split(" ").length>1
-    newArray << array[counter].split(" ").first
-  else
-    newArray << array[counter].upcase
-  end
+  #if array[counter].split(" ").length>1
+  #  newArray << array[counter].split(" ").first
+  #else
+  #  newArray << array[counter].upcase
+  #end
+  newArray << yield array[counter]
   counter+=1
   end
 newArray
